c567753315322a0cd03bc10e3c3f62d943e11cbc
1shera1iyev1c <1developer1ccc@gmail.com>
2023-12-20 23:50:30 +0500

    Calculator

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './index.css'
 
 const Calculator = () => {
   const [displayValue, setDisplayValue] = useState('0');
@@ -40,6 +41,20 @@ const Calculator = () => {
               onClick={() => handleButtonClick('7')}
             />
             <input
+              name="eight"
+              type="button"
+              value="8"
+              className="link sakkiz"
+              onClick={() => handleButtonClick('8')}
+            />
+            <input
+              name="nine"
+              type="button"
+              value="9"
+              className="link toqqiz"
+              onClick={() => handleButtonClick('9')}
+            />
+            <input
               name="times"
               type="button"
               value="/"
@@ -48,6 +63,87 @@ const Calculator = () => {
             />
           </div>
           <div className="list">
+            <input
+              name="four"
+              type="button"
+              value="4"
+              className="link tor"
+              onClick={() => handleButtonClick('4')}
+            />
+            <input
+              name="five"
+              type="button"
+              value="5"
+              className="link besh"
+              onClick={() => handleButtonClick('5')}
+            />
+            <input
+              name="six"
+              type="button"
+              value="6"
+              className="link olti"
+              onClick={() => handleButtonClick('6')}
+            />
+            <input
+              name="times"
+              type="button"
+              value="*"
+              className="operator link kopay"
+              onClick={() => handleButtonClick('*')}
+            />
+          </div>
+          <div className="list">
+            <input
+              name="one"
+              type="button"
+              value="1"
+              className="link bir"
+              onClick={() => handleButtonClick('1')}
+            />
+            <input
+              name="two"
+              type="button"
+              value="2"
+              className="link ikki"
+              onClick={() => handleButtonClick('2')}
+            />
+            <input
+              name="three"
+              type="button"
+              value="3"
+              className="link uch"
+              onClick={() => handleButtonClick('3')}
+            />
+            <input
+              name="times"
+              type="button"
+              value="-"
+              className="operator link minus"
+              onClick={() => handleButtonClick('-')}
+            />
+          </div>
+          <div className="list">
+            <input
+              name="."
+              type="button"
+              value="."
+              className="link tochka"
+              onClick={() => handleButtonClick('.')}
+            />
+            <input
+              name="zero"
+              type="button"
+              value="0"
+              className="link nol"
+              onClick={() => handleButtonClick('0')}
+            />
+            <input
+              name="times"
+              type="button"
+              value="+"
+              className="link plus"
+              onClick={() => handleButtonClick('+')}
+            />
             <input
               name="times"
               type="button"
